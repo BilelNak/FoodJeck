59c9f1ef4327fef12d7dfbdd75b6527c6cf536e6
unknown <nakti.bilel@gmail.com>
2022-10-01 16:11:02 +0100

    Initial version of platdb generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/foodjeck/platmanager/config/dbmigrations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MongoDB database migrations using Mongock.
+ */
+package com.foodjeck.platmanager.config.dbmigrations;
---
+++ src/main/java/com/foodjeck/platmanager/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.foodjeck.platmanager.web.rest.vm;
